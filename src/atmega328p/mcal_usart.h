e4e6c0d1677e2da0c801789f73462a5181975461
Kai Berggold <mail@kaiberggold.de>
2024-03-03 00:15:37 +0000

    formatter

--- src/atmega328p/mcal_usart.h
+++ src/atmega328p/mcal_usart.h
@@ -1,30 +1,21 @@
 #ifndef MCAL_USART
-  #define MCAL_USART
+#define MCAL_USART
 
-#include <cstdint>
-#include <array>
+#include "mcal_digital_port.h"
 #include "mcal_reg.h"
 #include "mcal_reg_access.h"
-#include "mcal_digital_port.h"
 #include "mcal_utils.h"
+#include <array>
 #include <cstdint>
 
-namespace hal
-{
-    class McalUsart
-       {
-        private:
-    
-            
+namespace hal {
+class McalUsart {
+private:
+public:
+  void init(const std::uint32_t baud_rate);
+  void transmit_byte_no_blocking(std::uint8_t data);
+  bool buffer_is_empty();
+};
 
-            
-            
-        public:
-
-            void init(const std::uint32_t baud_rate);
-            void transmit_byte_no_blocking(std::uint8_t data);
-            bool buffer_is_empty();
-        };
-
-}
+} // namespace hal
 #endif // MCAL_USART
