34658a8d9d048f03f8b07b0723da7d19108d81bb
Kai Berggold <mail@kaiberggold.de>
2024-04-16 11:31:48 +0200

    i2c hw working again

--- src/atmega328p/hal_i2c_com.h
+++ src/atmega328p/hal_i2c_com.h
@@ -4,33 +4,32 @@
 
 namespace hal
 {
-    template <typename addr_t, typename reg_t>
+    template <typename addr_t, typename reg_t, reg_t bus_idx, std::uint32_t freq>
     struct HalI2CCom
     {
 
-        static void start(reg_t bus_idx)
+        static void start()
         {
             HalRegAccess<addr_t, reg_t, hal::TWCR[bus_idx]>::reg_set_bits(hal::TWINT, hal::TWEN, hal::TWSTA);
         }
-        static void stop(reg_t bus_idx)
+        static void stop()
         {
             HalRegAccess<addr_t, reg_t, hal::TWCR[bus_idx]>::reg_set_bits(hal::TWINT, hal::TWEN, hal::TWSTO);
         }
-        static void send(reg_t bus_idx, reg_t data)
+        static void send(reg_t data)
         {
             HalRegAccess<addr_t, reg_t, hal::TWDR[bus_idx]>::reg_set(static_cast<reg_t>(data));
             HalRegAccess<addr_t, reg_t, hal::TWCR[bus_idx]>::reg_set_bits_only(hal::TWINT, hal::TWEN);
         }
-        static bool transmission_active(reg_t bus_idx)
+        static bool transmission_active()
         {
             // TODO: reg_bit_set()
             return !hal::reg_bit_set(hal::TWCR[bus_idx], hal::TWINT);
         }
-        template <reg_t bus_idx, std::uint32_t freq>
         static void init()
         {
             // Prescaler 1. TODO: adjustable prescalers
-            // HalRegAccess<addr_t, reg_t, hal::TWSR[bus_idx]>::reg_reset_bits(hal::TWPS0, hal::TWPS1);
+            HalRegAccess<addr_t, reg_t, hal::TWSR[bus_idx]>::reg_reset_bits(hal::TWPS0, hal::TWPS1);
             // set speed. TODO: adjustable prescalers
             HalRegAccess<addr_t, reg_t, hal::TWBR[bus_idx]>::reg_set(static_cast<reg_t>(((F_CPU) / (freq)) - UINT32_C(16)) / UINT32_C(2));
             //  enable
